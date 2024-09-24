1516db6c39e62c80ef1166529219c7fbb23a5794
EUNSEMIN <114456579+EUNSEMIN@users.noreply.github.com>
2024-09-24 15:38:03 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    int min = 60*a+b+c;
+    
+    int hour = (min/60)%24;
+    int minute = min%60;
+    
+    cout<<hour<<" "<<minute;
+    
+    return 0;
+}
