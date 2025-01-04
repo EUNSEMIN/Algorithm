80fd3fa25058c52da7a81872fdf539610cd35438
EUNSEMIN <114456579+EUNSEMIN@users.noreply.github.com>
2025-01-04 18:26:08 +0900

    [level 1] Title: 모의고사, Time: 0.09 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -4,30 +4,32 @@
 
 using namespace std;
 
-vector<int> vec1 = {1,2,3,4,5};
-vector<int> vec2 = {2,1,2,3,2,4,2,5};
-vector<int> vec3 = {3,3,1,1,2,2,4,4,5,5};
-
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> count(3,0);
+    vector<int> count(4,0);
     
+    // 찍는 방식 기록
+    vector<int> a1 = {1,2,3,4,5};
+    vector<int> a2 = {2,1,2,3,2,4,2,5};
+    vector<int> a3 = {3,3,1,1,2,2,4,4,5,5};
+    
+    // 정답 대조
     for(int i=0; i<answers.size(); i++){
-        if(answers[i] == vec1[i%vec1.size()])
-            count[0]++;
-        if(answers[i] == vec2[i%vec2.size()])
-            count[1]++;
-        if(answers[i] == vec3[i%vec3.size()])
-            count[2]++;
+        if(answers[i]==a1[i%5]) count[1]++;
+        if(answers[i]==a2[i%8]) count[2]++;
+        if(answers[i]==a3[i%10]) count[3]++;
     }
     
-    int max_score = *max_element(count.begin(), count.end());
+    // 가장 높은 점수 
+    int max = *max_element(count.begin(), count.end());
     
-    for(int i=0; i<3; i++){
-        if(count[i] == max_score){
-            answer.push_back(i+1);
-        }
+    // answer에 1등 기록
+    for(int i=1;i<count.size(); i++){
+        if(max == count[i]) answer.push_back(i);
     }
+    
+    // 정렬
+    sort(answer.begin(), answer.end());
     
     return answer;
 }
